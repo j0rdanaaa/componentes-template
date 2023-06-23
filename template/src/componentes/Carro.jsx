4562afa37b8b3557960abae6bb704496aa5008cc
j0rdanaaa <j0rdanaaa@gmail.com>
2023-06-23 15:24:14 -0300

    template componentes

---
+++ template/src/componentes/Carro.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+ const Carro = () => {
+    return (
+        <>
+        <h2 className="carros">Fusca</h2>
+        <ul>
+            <li>Cor: Amarelo</li>
+            <li>Ano: 1977</li>
+            <li>Flex: Não</li>
+            </ul>
+        </>
+    );
+};
+
+export default Carro;
---
+++ template/src/componentes/Garagem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Carro from "./Carro";
+
+const Garagem = () => {
+    return (
+        <>
+        <h1 className="garagem">Garagem de Jordana</h1>
+        <Carro/>
+        <Carro/>
+        <Carro/>
+        <Carro/>
+        </>
+    )
+}
+
+export default Garagem;
